cbda8f95f95200746510e0375dac74b5cf99beb0
williamabreu <williamsantos_00@hotmail.com>
2018-12-04 11:13:39 -0200

    Fixada rota para listagem all

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const arCtrl = require('../controllers/arController');
 const luzCtrl = require('../controllers/luzController');
+const allCtrl = require('../controllers/allController');
 
 router.get('/api/list/ar', arCtrl.list);
 router.get('/api/list/luz', luzCtrl.list);
+router.get('/api/list/all', allCtrl.list);
 
 module.exports = router;
